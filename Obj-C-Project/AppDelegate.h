faff9156c242b1ceeba63d88bffbab30854a4c80
shaoliangliang <shao_liangliang@163.com>
2018-06-08 10:18:07 +0800

    init project

---
+++ Obj-C-Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Obj-C-Project
+//
+//  Created by 邵亮亮 on 2018/6/8.
+//  Copyright © 2018年 shaoliangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
